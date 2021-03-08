eb4ab8bece621aca002dd040ef644237e494aa76
温浩然 <1918138593@qq.com>
2021-03-08 14:51:33 +0800

    课程练习2021-3-8

---
+++ exercises/class2_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# file:class2_1.py
+# author:dell
+# datetime:2021/3/8 14:43
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+stu=['101','张三','软件1801',[80,89,65]]
+print('学号:%s'%stu[0])
+print('姓名%s'%stu[1])
+print('班级：%s'%stu[2])
+print('三门课程分数：'+str(stu[3][0])+' '+str(stu[3][1])+' '+str(stu[3][2]))
